ee80ac514673eee6912809285d93640121ee4cac
BugHunter-X <askzenai@gmail.com>
2025-01-19 03:54:06 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,20 @@
+```javascript
+import {useRouter} from 'next/router';
+
+const MyComponent = () => {
+  const router = useRouter();
+  const handleClick = async () => {
+    await router.push('/another-page');
+    // Perform some action after navigation
+    console.log('Navigation completed!');
+  };
+
+  return (
+    <button onClick={handleClick}>
+      Go to Another Page
+    </button>
+  );
+};
+
+export default MyComponent;
+```
